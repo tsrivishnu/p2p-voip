0a02fb48166d31ba9db8deed45f3ddc0760c6f03
Sri Vishnu Totakura <Munna@Sri-Vishnu-Totakuras-MacBook-Pro.local>
2015-06-22 13:16:58 +0200

    Diffie-Hellman session key manager class with javadocs and example

---
+++ SourceCode/P2P_VOIP_App/src/edu/tum/p2p/group20/voip/dh/Config.java
@@ -0,0 +1,10 @@
+package edu.tum.p2p.group20.voip.dh;
+
+/**
+ * @author Sri Vishnu Totakura <srivishnu@totakura.in>
+ *
+ */
+public class Config {
+	public static int bitSize = 512;
+	public static String algorithm = "DH";
+}
