1b20d968a4831c72e796e266ba7c794e4d1b4dd7
Sri Vishnu Totakura <Munna@Sri-Vishnu-Totakuras-MacBook-Pro.local>
2015-08-23 12:46:39 +0200

    kxSimulator refactor and docs

--- SourceCode/P2P_VOIP_App/src/edu/tum/p2p/group20/voip/intraPeerCom/KXSimulator.java
+++ SourceCode/P2P_VOIP_App/src/edu/tum/p2p/group20/voip/intraPeerCom/KXSimulator.java
@@ -1,33 +1,22 @@
 package edu.tum.p2p.group20.voip.intraPeerCom;
 
-import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.OutputStream;
-import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.net.UnknownHostException;
-import java.security.InvalidKeyException;
-import java.security.KeyPair;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.security.SignatureException;
 import java.util.Arrays;
 import java.util.Scanner;
 
 import edu.tum.p2p.group20.voip.config.ConfigParser;
-import edu.tum.p2p.group20.voip.crypto.RSA;
-import edu.tum.p2p.group20.voip.intraPeerCom.messages.dht.Put;
 
 public class KXSimulator {
-	
+
 	private static ServerSocket serverSocket;
 	public static Socket socket;
-	
+
 	public static OutputStream out;
 	public static InputStream in;
 	public static Scanner userIn;
@@ -38,48 +27,56 @@ public class KXSimulator {
 
 	public static void main(String[] args) throws Exception {
 
-        try {
-        	parser = ConfigParser.getInstance("lib/test_app_config.ini");
-        	
-        	// Make sure for testing, the DHT and KX port are same in the config file!
-        	portNumber = parser.getKxPort();
-        	
-        	serverSocket = new ServerSocket(portNumber);
+		try {
+			parser = ConfigParser.getInstance("lib/test_app_config.ini");
 
-	        	socket = serverSocket.accept();
-		        out = socket.getOutputStream();
-	        	in = socket.getInputStream();
-	        	userIn = new Scanner(System.in); 
-	        	
-	        	byte[] receivedMessage;
-	        	
-	        	receiveMessagesLoop: while(readIncomingMessage() != null) {
-	        		String userMessage = "What do you want to do?";
-	        		userMessage += "\n1.Receive next message";
-	        		userMessage += "\n2.Send IN Tunnel Ready";
-	        		userMessage += "\n3.Send OUT Tunnel Ready";
-	        		userMessage += "\n4.Send KX_ERROR";
-	        		userMessage += "\n5.Send nothing";
-	        		userMessage += "\n6.BREAK";
-	                System.out.println(userMessage);
-	                switch (userIn.nextLine()) {
+			// Make sure for testing, the DHT and KX port are same in the config
+			// file!
+			portNumber = parser.getKxPort();
+
+			serverSocket = new ServerSocket(portNumber);
+
+			socket = serverSocket.accept();
+			out = socket.getOutputStream();
+			in = socket.getInputStream();
+			userIn = new Scanner(System.in);
+
+			receiveMessagesLoop: while (readIncomingMessage() != null) {
+				String userMessage = "What do you want to do?";
+				userMessage += "\n1.Receive next message";
+				userMessage += "\n2.Send IN Tunnel Ready";
+				userMessage += "\n3.Send OUT Tunnel Ready";
+				userMessage += "\n4.Send KX_ERROR";
+				userMessage += "\n5.Send nothing";
+				userMessage += "\n6.BREAK";
+				System.out.println(userMessage);
+				
+				switch (userIn.nextLine()) {
 					case "2":
+						// For IN tunnel ready, destination ips can be empty
 						sendTunnelReady(new byte[4], new byte[16]);
 						break;
 					case "3":
-						// This is the destination Ip the caller uses to reach callee,
-						//	Since we are simulating this app, there is no real tunnel. So, the 
-						//	destination Ip for the caller should be the ip of the receiver in our case.
-						// 	As this is running in a local environment, you must have set the
-						//	TUN_IP on the callee's machine to callee's ip address. 
-						//	So, that becomes the destinationIP.
-						byte[] destinationIp = InetAddress.getByName(parser.getTunIP()).getAddress();
+						// This is the destination Ip the caller uses to reach
+						// callee, Since we are simulating this app, there is 
+						// no real tunnel. So, the destination Ip for the
+						// caller should be the ip of the receiver in our case.
+						// As this is  running in a local environment, you
+						// must have set the TUN_IP on the callee's machine to
+						// callee's ip address. So, that becomes the
+						// destinationIP.
 						
+						byte[] destinationIp = InetAddress.getByName(
+							parser.getTunIP()
+						).getAddress();
+
 						sendTunnelReady(
-							destinationIp, 
-							InetAddress.getByName("3ffe:2a00:100:7031::1").getAddress()
+							destinationIp,
+							InetAddress.getByName("3ffe:2a00:100:7031::1")
+								.getAddress()
 						);
-						break;					
+						
+						break;
 					case "4":
 						sendErrorReply("MSG_KX_ERROR");
 						break;
@@ -87,197 +84,123 @@ public class KXSimulator {
 						break receiveMessagesLoop;
 					default:
 						break;
-					}
-        	}          
+				}
+			}
 
-        } catch (IOException e) {
-            System.out.println("Exception caught when trying to connect on port " + portNumber);
-            System.out.println(e.getMessage());
-        }
+		} catch (IOException e) {
+			System.out
+					.println("Exception caught when trying to connect on port "
+							+ portNumber);
+			System.out.println(e.getMessage());
+		}
 	}
-	
+
 	private static byte[] readIncomingMessage() throws IOException {
-		byte[] buff = new byte[2];		
-    	// First read the length
-        in.read(buff, 0, buff.length);
-        
-        short incomingSize = Helper.shortFromNetworkOrderedBytes(buff);
-        incomingSize = (short) (incomingSize - 2); // Cause two bytes are already read.
-        byte[] incomingBytes = new byte[incomingSize];
-        in.read(incomingBytes, 0, incomingSize);
-        
-        ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
-        byteStream.write(buff);
-        byteStream.write(incomingBytes);
-        
-        short messageCode = Helper.shortFromNetworkOrderedBytes(
-			Arrays.copyOfRange(byteStream.toByteArray(), 2, 4)
-		);
-        
-        System.out.println("Received Message: " + MessagesLegend.nameForCode(messageCode));
-        lastReceivedMessageName = MessagesLegend.nameForCode(messageCode);
-        lastReceivedMessage = byteStream.toByteArray();
-        
-        return lastReceivedMessage;
-	}
-	
-	/**
-	 * Send a Dummy DHT GET REPLY for the received DHT GET message.
-	 * This is only a Dummy, the content in this makes no sense.
-	 * 
-	 * @throws IOException
-	 * @throws NoSuchAlgorithmException 
-	 * @throws SignatureException 
-	 * @throws InvalidKeyException 
-	 */
-	private static void sendDhtDummyGetReply() throws IOException, NoSuchAlgorithmException, InvalidKeyException, SignatureException {
 
-		byte[] pseudoId = Arrays.copyOfRange(lastReceivedMessage, 4, 36);
-		byte[] messageCode = Helper.networkOrderedBytesFromShort(
-				(short) MessagesLegend.codeForName("MSG_DHT_GET_REPLY")
-			);
-		
-		// xChangePointInfo
-		MessageDigest md = MessageDigest.getInstance("SHA-256");
-		md.update("exchangePoint2".getBytes());
-		byte[] peer2Id = md.digest();
-		byte[] peer2KxPort = Helper.networkOrderedBytesFromShort((short) 3001);
-		byte[] peer2reserved = new byte[2];
-		byte[] peer2ip = InetAddress.getByName("192.168.2.2").getAddress();
-		byte[] peer2ipv6 = InetAddress.getByName("3ffe:2a00:100:7031::1").getAddress();
-		
-		ByteArrayOutputStream xchangeStream = new ByteArrayOutputStream();
-		xchangeStream.write(peer2KxPort);
-		xchangeStream.write(peer2reserved);
-		xchangeStream.write(peer2Id);
-		xchangeStream.write(peer2ip);
-		xchangeStream.write(peer2ipv6);
-		byte[] xchangePointInfo = xchangeStream.toByteArray();
-		
-		KeyPair hostKeyPair = RSA.getKeyPairFromFile(parser.getTestReceiverRsaKeyPair());
-		
-		// We use a sample put to get all the signature and proper content encapsulation
-		Put samplePut = new Put(pseudoId,(short) 1000, 2, hostKeyPair, xchangePointInfo);
+		byte[] buff = new byte[2];
+		// First read the length
+		in.read(buff, 0, buff.length);
 
-		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-		outputStream.write(messageCode);
-		outputStream.write(pseudoId);
-		//content
-		outputStream.write(samplePut.byteValues.get("publicKey"));
-		outputStream.write(samplePut.byteValues.get("pseudoIdToBeSigned"));
-		outputStream.write(samplePut.byteValues.get("xchangePointInfoForKx"));
-		outputStream.write(samplePut.byteValues.get("signature"));
-		
-		byte[] fullDhtReplyMessage = prependSizeForMessage(outputStream.toByteArray());		
-		out.write(fullDhtReplyMessage, 0, fullDhtReplyMessage.length);
+		short incomingSize = Helper.shortFromNetworkOrderedBytes(buff);
+		incomingSize = (short) (incomingSize - 2); // Cause two bytes are
+													// already read.
+		byte[] incomingBytes = new byte[incomingSize];
+		in.read(incomingBytes, 0, incomingSize);
+
+		ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
+		byteStream.write(buff);
+		byteStream.write(incomingBytes);
+
+		short messageCode = Helper.shortFromNetworkOrderedBytes(Arrays
+				.copyOfRange(byteStream.toByteArray(), 2, 4));
+
+		System.out.println("Received Message: "
+				+ MessagesLegend.nameForCode(messageCode));
+		lastReceivedMessageName = MessagesLegend.nameForCode(messageCode);
+		lastReceivedMessage = byteStream.toByteArray();
+
+		return lastReceivedMessage;
 	}
-	
+
 	/**
-	 * Prepends the size field for a message after calculating the size of the message and
-	 * returns the full byte array of the full encapsulated message
+	 * Prepends the size field for a message after calculating the size of the
+	 * message and returns the full byte array of the full encapsulated message
 	 * 
 	 * @param message
 	 * @return full message with size as byte array
 	 * @throws IOException
 	 */
-	private static byte[] prependSizeForMessage(byte[] message) throws IOException {
-		byte[] size = Helper.networkOrderedBytesFromShort((short) (message.length + 2)); //+2 for size of size field
+	private static byte[] prependSizeForMessage(byte[] message)
+			throws IOException {
+		
+		byte[] size = Helper.networkOrderedBytesFromShort(
+			(short) (message.length + 2)
+		); // +2 for the size of the Size field
+		
 		ByteArrayOutputStream outputStream2 = new ByteArrayOutputStream();
 		outputStream2.write(size);
 		outputStream2.write(message);
 		byte[] fullMessage = outputStream2.toByteArray();
-		return fullMessage;		
+		
+		return fullMessage;
 	}
-	
+
 	/**
-	 * Send a Sample DHT TRACE REPLY message based on the last message received
-	 * 
-	 * @throws NoSuchAlgorithmException
-	 * @throws IOException
-	 */
-	private static void sendDhtTraceReply() throws NoSuchAlgorithmException, IOException {
-		byte[] size;
-		byte[] key = Arrays.copyOfRange(lastReceivedMessage, 4, 36);
-		byte[] messageCode = Helper.networkOrderedBytesFromShort(
-				(short) MessagesLegend.codeForName("MSG_DHT_TRACE_REPLY")
-			);
-		
-		MessageDigest md = MessageDigest.getInstance("SHA-256");
-		md.update("exchangePoint1".getBytes());
-		byte[] peer1Id = md.digest();
-		byte[] peer1KxPort = Helper.networkOrderedBytesFromShort((short) 3001);
-		byte[] peer1reserved = new byte[2];
-		byte[] peer1ip = InetAddress.getByName("192.168.2.1").getAddress();
-		byte[] peer1ipv6 = InetAddress.getByName("FEDC:BA98:7654:3210:FEDC:BA98:7654:3210").getAddress();
-		
-		md.update("exchangePoint2".getBytes());
-		byte[] peer2Id = md.digest();
-		byte[] peer2KxPort = Helper.networkOrderedBytesFromShort((short) 3001);
-		byte[] peer2reserved = new byte[2];
-		byte[] peer2ip = InetAddress.getByName("192.168.2.2").getAddress();
-		byte[] peer2ipv6 = InetAddress.getByName("3ffe:2a00:100:7031::1").getAddress();
-		
-		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-		outputStream.write(messageCode);
-		outputStream.write(key);
-		
-		outputStream.write(peer2Id);
-		outputStream.write(peer2KxPort);
-		outputStream.write(peer2reserved);
-		outputStream.write(peer2ip);
-		outputStream.write(peer2ipv6);
-		
-		outputStream.write(peer1Id);
-		outputStream.write(peer1KxPort);
-		outputStream.write(peer1reserved);
-		outputStream.write(peer1ip);
-		outputStream.write(peer1ipv6);
-		
-		byte[] fullDhtReplyMessage = prependSizeForMessage(outputStream.toByteArray());		
-		out.write(fullDhtReplyMessage, 0, fullDhtReplyMessage.length);
-	}
-	
-	/**
-	 * Sends a tunnel ready reply to the requester with the give destinationIpAddress value
+	 * Sends a tunnel ready reply to the requester with the give
+	 * destinationIpAddress value
 	 * 
 	 * @param destinationIpAddress
 	 * @throws IOException
 	 */
-	private static void sendTunnelReady(byte[] destinationIpv4, byte[] destinationIpv6) throws IOException {		
+	private static void sendTunnelReady(byte[] destinationIpv4,
+			byte[] destinationIpv6) throws IOException {
+
 		byte[] key = Arrays.copyOfRange(lastReceivedMessage, 8, 40);
 
-		byte[] messageCode = Helper.networkOrderedBytesFromShort(
+		byte[] messageCode = Helper
+			.networkOrderedBytesFromShort(
 				(short) MessagesLegend.codeForName("MSG_KX_TN_READY")
 			);
-		byte[] reserved = new byte[4];
 		
+		byte[] reserved = new byte[4];
+
 		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
 		outputStream.write(messageCode);
 		outputStream.write(key);
 		outputStream.write(reserved);
 		outputStream.write(destinationIpv4);
 		outputStream.write(destinationIpv6);
-		
-		byte[] fullTunnelReadyMessage = prependSizeForMessage(outputStream.toByteArray());		
+
+		byte[] fullTunnelReadyMessage = prependSizeForMessage(outputStream
+				.toByteArray());
 		out.write(fullTunnelReadyMessage, 0, fullTunnelReadyMessage.length);
 	}
-	
+
+	/**
+	 * Sends an Error reply.
+	 * 
+	 * @param messageName
+	 * @throws Exception
+	 */
 	private static void sendErrorReply(String messageName) throws Exception {
+
 		byte[] key = Arrays.copyOfRange(lastReceivedMessage, 4, 36);
-		
-		byte[] messageCode = Helper.networkOrderedBytesFromShort(
+
+		byte[] messageCode = Helper
+			.networkOrderedBytesFromShort(
 				(short) MessagesLegend.codeForName(messageName)
 			);
 		byte[] reserved = new byte[2];
 		byte[] request_type = Helper.networkOrderedBytesFromShort((short) 10);
-		
+
 		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
 		outputStream.write(messageCode);
 		outputStream.write(request_type);
 		outputStream.write(reserved);
 		outputStream.write(key);
-		
-		byte[] fullDhtErrorMessage = prependSizeForMessage(outputStream.toByteArray());
+
+		byte[] fullDhtErrorMessage = prependSizeForMessage(outputStream
+				.toByteArray());
 		out.write(fullDhtErrorMessage, 0, fullDhtErrorMessage.length);
 	}
 }
